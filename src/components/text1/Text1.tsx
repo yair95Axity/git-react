dbbba6aefd7d192fa820a6de86045fdd0a6b55a5
Juan Trejo <juan.trejor@axity.com>
2025-05-16 16:07:52 -0600

    Es menor o mayor de edad

--- src/components/text1/Text1.tsx
+++ src/components/text1/Text1.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
- 
+
 export const Text1 = () => {
   const edad = 27;
-    return (
-    <div>Mi edad: {edad}</div>
+  return (
+    <div>Mi edad: {edad}  es {edad > 18 ? 'mayor de edad' : 'menor de edad'} </div>
   )
 }
