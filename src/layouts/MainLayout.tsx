5d010eb253cdde7016a229f197e0703d9238f5f9
Yair Marin <edgar.marin@axity.com>
2025-05-16 13:56:28 -0600

    add main components

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,13 @@
+import { Counter } from "../components/counter/Counter"
+import styles from './MainLayout.module.css'
 
 
 export const MainLayout = () => {
     return (
-        <div>
+        <div className={styles.mainLayout}>
+            <h1>Main Layout</h1>
+
+            <Counter />
         </div>
     )
 }
